8d79e554f38f966085f8792a6d58fe6c582037fa
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-27 20:45:57 -0800

    pan and zoom canvas

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,5 +1,6 @@
 const canvasContainer = document.getElementById('canvas-container')
 const canvas = <HTMLCanvasElement>document.createElement('canvas')
+canvas.id = 'canvas'
 canvas.width = canvasContainer.offsetWidth - 2
 canvas.height = canvasContainer.offsetHeight - 2
 canvasContainer.appendChild(canvas)
@@ -22,55 +23,211 @@ class Cell {
   }
 }
 
-const allCellAliveStatesCombinationsByThree = [
-  [false, false, false],
-  [false, false, true],
-  [false, true, false],
-  [false, true, true],
-  [true, false, false],
-  [true, false, true],
-  [true, true, false],
-  [true, true, true],
-]
+let cameraOffset = { x: canvasContainer.offsetWidth / 2, y: canvasContainer.offsetHeight / 2 }
+let cameraZoom = 1
+let MAX_ZOOM = 5
+let MIN_ZOOM = 0.1
+let SCROLL_SENSITIVITY = 0.0005
 
-const cells = async () => {
-  let x = 0
-  for (let i = 0; i < allCellAliveStatesCombinationsByThree.length; i++) {
-    for (let j = 0; j < allCellAliveStatesCombinationsByThree[i].length; j++) {
-      // Draw all cells vertically
+function draw() {
+  canvas.width = canvasContainer.offsetWidth
+  canvas.height = canvasContainer.offsetHeight
+  // Translate to the canvas centre before zooming - so you'll always zoom on what you're looking directly at
+  ctx.translate(canvasContainer.offsetWidth / 2, canvasContainer.offsetHeight / 2)
+  ctx.scale(cameraZoom, cameraZoom)
+  ctx.translate(-canvasContainer.offsetWidth / 2 + cameraOffset.x, -canvasContainer.offsetHeight / 2 + cameraOffset.y)
+  ctx.clearRect(0, 0, canvasContainer.offsetWidth, canvasContainer.offsetHeight)
+  ctx.fillStyle = '#991111'
+  drawRect(-50, -50, 100, 100)
 
-      let separator = 0
-      // Separator between cells sets
-      console.log(i, j)
+  ctx.fillStyle = '#eecc77'
+  drawRect(-35, -35, 20, 20)
+  drawRect(15, -35, 20, 20)
+  drawRect(-35, 15, 70, 20)
 
-      if (j === 2) {
-        separator = (canvas.width - allCellAliveStatesCombinationsByThree.length * CELL_WIDTH * 3) / (allCellAliveStatesCombinationsByThree.length - 1)
-      }
+  ctx.fillStyle = '#fff'
+  drawText('Simple Pan and Zoom Canvas', -255, -100, 32, 'courier')
 
-      ctx.fillStyle = allCellAliveStatesCombinationsByThree[i][j] ? 'black' : 'white'
-      // Spread the cells horizontally
-      ctx.fillRect(x, 1, CELL_WIDTH - 1, CELL_HEIGHT - 1)
-      x = x + CELL_WIDTH + separator
-      await new Promise((resolve) => setTimeout(resolve, 100))
-    }
+  ctx.rotate((-31 * Math.PI) / 180)
+  ctx.fillStyle = `#${(Math.round(Date.now() / 40) % 4096).toString(16)}`
+  drawText('Now with touch!', -110, 100, 32, 'courier')
+
+  ctx.fillStyle = '#fff'
+  ctx.rotate((31 * Math.PI) / 180)
+
+  drawText('Wow, you found me!', -260, -2000, 48, 'courier')
+
+  requestAnimationFrame(draw)
+}
+
+// Gets the relevant location from a mouse or single touch event
+function getEventLocation(e: any) {
+  if (e.touches && e.touches.length == 1) {
+    return { x: e.touches[0].clientX, y: e.touches[0].clientY }
+  } else if (e.clientX && e.clientY) {
+    return { x: e.clientX, y: e.clientY }
   }
 }
 
-cells()
-
-// Draw rectangle
-// ctx.fillStyle = "black";
-// ctx.fillRect(0, 0, CELL_WIDTH, CELL_HEIGHT);
-// ctx.strokeStyle = "black";
-// ctx.strokeRect(20, 0, CELL_WIDTH, CELL_HEIGHT);
-// ctx.strokeRect(20, 20, CELL_WIDTH, CELL_HEIGHT);
-
-function drawFrame() {
-  // setTimeout(function () {
-  //   requestAnimationFrame(() => drawFrame());
-  //   // animating/drawing code goes here
-  // }, 1000 / parseInt(speedRange.value));
+function drawRect(x: number, y: number, width: number, height: number) {
+  ctx.fillRect(x, y, width, height)
 }
-drawFrame()
 
-export {}
+function drawText(text: string, x: number, y: number, size: number, font: string) {
+  ctx.font = `${size}px ${font}`
+  ctx.fillText(text, x, y)
+}
+
+let isDragging = false
+let dragStart = { x: 0, y: 0 }
+
+function onPointerDown(e: Event) {
+  isDragging = true
+  dragStart.x = getEventLocation(e).x / cameraZoom - cameraOffset.x
+  dragStart.y = getEventLocation(e).y / cameraZoom - cameraOffset.y
+}
+
+function onPointerUp(e: Event) {
+  isDragging = false
+  initialPinchDistance = null
+  lastZoom = cameraZoom
+}
+
+function onPointerMove(e: Event) {
+  if (isDragging) {
+    cameraOffset.x = getEventLocation(e).x / cameraZoom - dragStart.x
+    cameraOffset.y = getEventLocation(e).y / cameraZoom - dragStart.y
+  }
+}
+
+function handleTouch(e: TouchEvent, singleTouchHandler: (e: TouchEvent) => void) {
+  if (e.touches.length == 1) {
+    singleTouchHandler(e)
+  } else if (e.type == 'touchmove' && e.touches.length == 2) {
+    isDragging = false
+    handlePinch(e)
+  }
+}
+
+let initialPinchDistance: any = null
+let lastZoom = cameraZoom
+
+function handlePinch(e: TouchEvent) {
+  e.preventDefault()
+
+  let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+  let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
+
+  // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+  let currentDistance = (touch1.x - touch2.x) ** 2 + (touch1.y - touch2.y) ** 2
+
+  if (initialPinchDistance == null) {
+    initialPinchDistance = currentDistance
+  } else {
+    adjustZoom(null, currentDistance / initialPinchDistance)
+  }
+}
+
+function adjustZoom(zoomAmount: number, zoomFactor: number) {
+  if (!isDragging) {
+    if (zoomAmount) {
+      cameraZoom += zoomAmount
+    } else if (zoomFactor) {
+      console.log(zoomFactor)
+      cameraZoom = zoomFactor * lastZoom
+    }
+
+    cameraZoom = Math.min(cameraZoom, MAX_ZOOM)
+    cameraZoom = Math.max(cameraZoom, MIN_ZOOM)
+
+    console.log(zoomAmount)
+  }
+}
+
+canvas.addEventListener('mousedown', onPointerDown)
+canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+canvas.addEventListener('mouseup', onPointerUp)
+canvas.addEventListener('touchend', (e) => handleTouch(e, onPointerUp))
+canvas.addEventListener('mousemove', onPointerMove)
+canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+canvas.addEventListener('wheel', (e) => adjustZoom(e.deltaY * SCROLL_SENSITIVITY, null))
+
+// Ready, set, go
+draw()
+
+// const allCellAliveStatesCombinationsByThree = [
+//   [false, false, false],
+//   [false, false, true],
+//   [false, true, false],
+//   [false, true, true],
+//   [true, false, false],
+//   [true, false, true],
+//   [true, true, false],
+//   [true, true, true],
+// ]
+
+// const allBinaryCombinationOfLength = (length: number) => {
+//   const result = []
+//   for (let i = 0; i < Math.pow(2, length); i++) {
+//     const binary = i.toString(2)
+//     const paddedBinary = '0'.repeat(length - binary.length) + binary
+//     result.push(paddedBinary.split('').map(Number))
+//   }
+//   return result
+// }
+
+// const asyncFunction = async () => {
+//   let x = 0
+//   // for (let i = 0; i < allCellAliveStatesCombinationsByThree.length; i++) {
+//   //   for (let j = 0; j < allCellAliveStatesCombinationsByThree[i].length; j++) {
+//   //     // Separator between cells sets
+//   //     let separator = 0
+//   //     if (j === 2) {
+//   //       separator = (canvas.width - allCellAliveStatesCombinationsByThree.length * CELL_WIDTH * 3) / (allCellAliveStatesCombinationsByThree.length - 1)
+//   //     }
+
+//   //     ctx.fillStyle = allCellAliveStatesCombinationsByThree[i][j] ? 'black' : 'white'
+//   //     // Spread the cells horizontally
+//   //     ctx.fillRect(x, 1, CELL_WIDTH - 1, CELL_HEIGHT - 1)
+//   //     x = x + CELL_WIDTH + separator
+//   //     await new Promise((resolve) => setTimeout(resolve, 100))
+//   //   }
+//   // }
+
+//   const allPossibleRules = allBinaryCombinationOfLength(allCellAliveStatesCombinationsByThree.length)
+
+//   for (let i = 0; i < allPossibleRules.length; i++) {
+//     for (let j = 0; j < allPossibleRules[i].length; j++) {
+//       ctx.fillStyle = allPossibleRules[i][j] ? 'black' : 'white'
+
+//       // Spread the cells horizontally
+//       ctx.fillRect(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH - 1, CELL_HEIGHT - 1)
+//     }
+//   }
+// }
+// asyncFunction()
+
+// // Draw rectangle
+// // ctx.fillStyle = "black";
+// // ctx.fillRect(0, 0, CELL_WIDTH, CELL_HEIGHT);
+// // ctx.strokeStyle = "black";
+// // ctx.strokeRect(20, 0, CELL_WIDTH, CELL_HEIGHT);
+// // ctx.strokeRect(20, 20, CELL_WIDTH, CELL_HEIGHT);
+
+// // function drawFrame() {
+// //   const p1 = ctx.transformedPoint(0, 0)
+// //   const p2 = ctx.transformedPoint(canvas.width, canvas.height)
+// //   console.log(p1, p2)
+
+// //   ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y)
+
+// //   // ctx.fillStyle = 'black'
+// //   // ctx.fillRect(0, 0, 40, 40)
+// //   setTimeout(function () {
+// //     requestAnimationFrame(() => drawFrame())
+// //     // animating/drawing code goes here
+// //   }, 1000 / parseInt(speedRange.value))
+// // }
+// // drawFrame()
+
+// export {}
--- src/controlPanel.ts
+++ src/controlPanel.ts
@@ -1,14 +1,8 @@
-const speedRange = <HTMLInputElement>(
-  document.querySelector("#cellular-automate-controls-sliders-speed-input")
-);
-const speedRangeValueElement = document.querySelector(
-  ".speed-range-value"
-) as HTMLSpanElement;
+const speedRange = <HTMLInputElement>document.querySelector('#cellular-automate-controls-sliders-speed-input')
+const speedRangeValueElement = document.querySelector('.speed-range-value') as HTMLSpanElement
 
-console.log("hello");
+speedRange.addEventListener('input', function () {
+  speedRangeValueElement.innerText = `${this.value}`
+})
 
-speedRange.addEventListener("input", function () {
-  speedRangeValueElement.innerText = `${this.value}`;
-});
-
-export {};
+export {}
